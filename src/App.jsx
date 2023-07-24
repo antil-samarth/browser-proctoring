6fec2447d3f4b815eb40c4b851a1517ae3f615a3
antil-samarth <antil.samarth@gmail.com>
2023-07-24 17:47:30 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import Webcam from "react-webcam";
+import cv from "@techstark/opencv-js";
+import { loadHaarFaceModels, detectHaarFace } from "./haarFaceDetection";
+import "./styles.css";
+
+export default function App() {
+  const [modelLoaded, setModelLoaded] = React.useState(false);
+  const [isActive, setIsActive] = React.useState(false);
+  const canvas = document.querySelector("canvas");
+  let pct = 0;
+  let count;
+
+  React.useEffect(() => {
+    loadHaarFaceModels().then(() => {
+      setModelLoaded(true);
+    });
+  }, []);
+
+  const webcamRef = React.useRef(null);
+  const imgRef = React.useRef(null);
+  const faceImgRef = React.useRef(null);
+
+  React.useEffect(() => {
+    if (!modelLoaded) return;
+
+    const detectFace = async () => {
+      if (!isActive){
+        canvas.style.display = "none";
+        return;
+      }
+
+      const imageSrc = webcamRef.current.getScreenshot();
+      if (!imageSrc) return;
+
+      return new Promise((resolve) => {
+        imgRef.current.src = imageSrc;
+        imgRef.current.onload = () => {
+          try {
+            const img = cv.imread(imgRef.current);
+            img, count = detectHaarFace(img);
+            cv.imshow(faceImgRef.current, img);
+
+            img.delete();
+            resolve();
+          } catch (error) {
+            console.log(error);
+            resolve();
+          }
+        };
+      });
+    };
+
+    let handle;
+    const nextTick = () => {
+      handle = setInterval(async () => {
+        await detectFace();
+      }, 1000/24);
+    };
+    nextTick();
+    const detectionStopTimeout = setTimeout(() => {
+      setIsActive(false);
+    }, 18000); // 180 seconds (3 minutes)
+
+    return () => {
+      clearInterval(handle);
+      clearTimeout(detectionStopTimeout);
+      pct = count
+      console.log(pct)
+    };
+
+    
+  }, [isActive]);
+
+  const handleStartFaceDetection = () => {
+    setIsActive(true);
+    canvas.style.display = "block";
+  };
+
+  return (
+    <div className="App">
+      <Webcam
+        ref={webcamRef}
+        className="webcam"
+        mirrored
+        screenshotFormat="image/jpeg"
+        audio={false}
+      />
+      <img className="inputImage" alt="input" ref={imgRef} />
+      <canvas className="outputImage" ref={faceImgRef} />
+      {!modelLoaded && <div>Loading Haar-cascade face model...</div>}
+      {!isActive && modelLoaded && (
+        <button onClick={handleStartFaceDetection}>Start Face Detection</button>
+      )}
+    </div>
+  );
+}
